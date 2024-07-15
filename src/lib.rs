52d9d5fc870226d08cc8d2e9f7c3c7684acea090
lemmih <lemmih@gmail.com>
2024-07-15 14:57:27 +0200

    basic cf worker template

---
+++ src/lib.rs
@@ -0,0 +1,97 @@
+use leptos::{component, server, spawn_local, view, IntoView, ServerFnError};
+use leptos_meta::*;
+
+#[server]
+pub async fn shouting_text(input: String) -> Result<String, ServerFnError> {
+    log::info!("Server-side fn: {}", &input);
+    // insert a simulated wait
+    Ok(input.to_ascii_uppercase())
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    provide_meta_context();
+    view! {
+        <p>Leptos</p>
+        <button on:click=move |_| {
+            spawn_local(async move {
+                let val = shouting_text(String::from("test")).await.ok();
+                log::info!("Got answer from server: {:?}", val);
+            });
+        }>Test</button>
+    }
+}
+
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    leptos::mount_to_body(App);
+}
+
+#[cfg(feature = "ssr")]
+mod ssr_imports {
+    use crate::App;
+    use axum::http::{HeaderValue, StatusCode};
+    use axum::{
+        extract::Path,
+        response::IntoResponse,
+        routing::{get, post},
+        Router,
+    };
+    use include_dir::{include_dir, Dir};
+    use leptos::*;
+    use leptos_axum::{generate_route_list, LeptosRoutes};
+    use worker::{event, Context, Env, HttpRequest, Result};
+
+    static PKG_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/pkg/");
+
+    async fn serve_static(Path(path): Path<String>) -> impl IntoResponse {
+        let mime_type = mime_guess::from_path(&path).first_or_text_plain();
+        let mut headers = axum::http::HeaderMap::new();
+        headers.insert(
+            axum::http::header::CONTENT_TYPE,
+            HeaderValue::from_str(mime_type.as_ref()).unwrap(),
+        );
+        match PKG_DIR.get_file(path) {
+            None => (StatusCode::NOT_FOUND, headers, "File not found.".as_bytes()),
+            Some(file) => (StatusCode::OK, headers, file.contents()),
+        }
+    }
+
+    fn router() -> Router {
+        let leptos_options = LeptosOptions::builder()
+            .output_name("client")
+            .site_pkg_dir("pkg")
+            .build();
+        let routes = generate_route_list(App);
+
+        // build our application with a route
+        let app: axum::Router<()> = Router::new()
+            .leptos_routes(&leptos_options, routes, App)
+            .route("/api/*fn_name", post(leptos_axum::handle_server_fns))
+            .route("/pkg/*file_name", get(serve_static))
+            .with_state(leptos_options);
+        app
+    }
+
+    #[event(start)]
+    fn register() {
+        server_fn::axum::register_explicit::<super::ShoutingText>();
+    }
+
+    #[event(fetch)]
+    async fn fetch(
+        req: HttpRequest,
+        _env: Env,
+        _ctx: Context,
+    ) -> Result<axum::http::Response<axum::body::Body>> {
+        _ = console_log::init_with_level(log::Level::Debug);
+        use tower_service::Service;
+
+        console_error_panic_hook::set_once();
+
+        Ok(router().call(req).await?)
+    }
+}
