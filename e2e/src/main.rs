4aa7c21cb6ba3b38d7791e5096f42cdcc2a7350c
lemmih <lemmih@gmail.com>
2024-07-18 09:09:25 +0200

    back to headless

--- e2e/src/main.rs
+++ e2e/src/main.rs
@@ -8,6 +8,7 @@ pub static CHROME_DRIVER_URL: &str = "http://127.0.0.1:9515";
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let mut cap = DesiredCapabilities::chrome();
+    cap.set_headless()?;
     let chrome = WebDriver::new(CHROME_DRIVER_URL, cap).await?;
     chrome.goto(WEBPAGE_URL).await?;
     let active = chrome.find(By::Tag("html")).await?;
